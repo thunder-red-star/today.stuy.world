048af2e3a6fbb5db2d4b9c26e5e193345ff0590c
ThunderRedStar <wvongphanith30@stuy.edu>
2022-05-04 09:25:14 -0400

    saikdlasilkjsd

--- src/index.js
+++ src/index.js
@@ -1,4 +1,45 @@
-const app = require('./app');
-const port = process.env.PORT || 3000;
+// Backend for Today.stuy.world
 
-app(port);
+const express = require('express');
+const app = express();
+
+const path = require('path');
+// const cors = require('cors');
+// const bcrypt = require('bcrypt');
+const http = require('http');
+const server = http.createServer(app);
+
+const socket = require('socket.io');
+const io = socket(server);
+
+const StuyUtils = require('stuyutils.js');
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+io.on('connection', (socket) => {
+    console.log('a user connected');
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+    });
+    socket.on('heartbeat', () => {
+        console.log('heartbeat');
+    });
+});
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+// Every second, send a heartbeat to the client with the bell information
+setInterval(() => {
+    io.emit('heartbeat', {
+        currTime: new Date().getTime(),
+        currentClass: StuyUtils.getCurrentClass(new Date()),
+        nextClass: StuyUtils.getNextClass(new Date()),
+    });
+}, 1000);
+
+// Listen on port 3000
+server.listen(3000, () => {
+    console.log('listening on *:3000');
+});
