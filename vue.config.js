e335c6591b182928af317a7f956ccd56c1f89f19
ThunderRedStar <wvongphanith30@stuy.edu>
2022-05-03 20:02:13 -0400

    webpacked

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,12 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  devServer: {
+    compress: true,
+    port: '8080',
+    allowedHosts: [
+      '.repl.co',
+      '.stuy.world'
+    ]
+  },
 })
