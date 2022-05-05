6b6d53dd5a386e725ce542979730b6569040f9a6
ThunderRedStar <wvongphanith30@stuy.edu>
2022-05-05 10:11:04 -0400

    asdasd

--- src/App.js
+++ src/App.js
@@ -4,6 +4,11 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 // Main app, import main view and hamburger menu
 import Landing from './features/Landing/Landing';
 import Home from './features/Home/Home';
+import Bells from "./features/Bells/Bells";
+import Announcements from "./features/Announcements/Announcements";
+import Links from "./features/Links/Links";
+import Credits from "./features/Credits/Credits";
+import Settings from "./features/Settings/Settings";
 
 // Don't forget to call renderImportAttributes
 function App() {
@@ -13,6 +18,11 @@ function App() {
         <Routes>
           <Route path="/" exact element={<Landing />} />
           <Route path="/home/*" exact element={<Home />} />
+            <Route path="/bells" component={Bells} />
+            <Route path="/announcements" component={Announcements} />
+            <Route path="/links" component={Links} />
+            <Route path="/credits" component={Credits} />
+            <Route path="/settings" component={Settings} />
         </Routes>
       </div>
     </BrowserRouter>
