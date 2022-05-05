1d6e3312b90417982ef7a767776b890213f06b74
ThunderRedStar <wvongphanith30@stuy.edu>
2022-05-05 09:43:25 -0400

    😭

--- src/App.js
+++ src/App.js
@@ -4,13 +4,15 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 // Main app, import main view and hamburger menu
 import Landing from './features/Landing/Landing';
 import Home from './features/Home/Home';
+
+// Don't forget to call renderImportAttributes
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
         <Routes>
-          <Route path="/" exact element={Landing} />
-          <Route path="/home" exact element={Home} />
+          <Route path="/" exact element={<Landing />} />
+          <Route path="/home" exact element={<Home />} />
         </Routes>
       </div>
     </BrowserRouter>
