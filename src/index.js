275c1ade8c642574816f6ce6690e6cf905940e53
ThunderRedStar <wvongphanith30@stuy.edu>
2022-05-04 22:15:30 -0400

    I KEEP USING THE WRONG VARS

--- src/index.js
+++ src/index.js
@@ -37,9 +37,9 @@ setInterval(() => {
         currTime: new Date().getTime(),
         currentClassName: currentClass.name,
         // Convert the class start time to a current time, then subtract that time from the current time to get the time since the class started.
-        currentClassTimeSince: new Date().getTime() - ThunderUtils.time.epochToCurrent(currentClass.startTime).getTime(),
+        currentClassTimeSince: new Date().getTime() - ThunderUtils.time.epochToCurrent(currentClass.start).getTime(),
         // Do the same for the end time but subtract in the opposite direction.
-        currentClassTimeUntil: ThunderUtils.time.epochToCurrent(currentClass.endTime).getTime() - new Date().getTime(),
+        currentClassTimeUntil: ThunderUtils.time.epochToCurrent(currentClass.end).getTime() - new Date().getTime(),
     });
 }, 1000);
 
