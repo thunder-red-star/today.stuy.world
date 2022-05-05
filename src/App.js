66ff0c51668cb2159c68a5926ec25b8fb638484a
ThunderRedStar <wvongphanith30@stuy.edu>
2022-05-05 09:35:48 -0400

    a

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 
 // Main app, import main view and hamburger menu
 import Landing from './features/Landing/Landing';
-
+import Home from './features/Home/Home';
 function App() {
   return (
-    <div className="App">
-      <Landing />
-    </div>
+    <Router>
+      <div className="App">
+        <Route exact path="/" component={Landing} />
+          <Route path="/home" component={Home} />
+      </div>
+    </Router>
   );
 }
 
---
+++ src/features/Home/Home.js
@@ -0,0 +1,13 @@
+// Landing page for the application
+import React from 'react';
+import ButtonLink from '../../components/Button/ButtonLink';
+
+export default class Landing extends React.Component {
+    render() {
+        return (
+            <div className="landing">
+                <h1>Welcome to the Home Page</h1>
+            </div>
+        );
+    }
+}
