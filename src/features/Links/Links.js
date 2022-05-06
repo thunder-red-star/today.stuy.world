edc4639a29260ded0699b39cd53ba4e55af02066
ThunderRedStar <wvongphanith30@stuy.edu>
2022-05-05 20:14:46 -0400

    missing stuff in links page

--- src/features/Links/Links.js
+++ src/features/Links/Links.js
@@ -1,10 +1,14 @@
 // Landing page for the application
 import React from 'react';
+import Navigation from "../Navigation/Navigation";
+import ThemeSetter from "../Theme/ThemeSetter";
 
 export default class Links extends React.Component {
     render() {
         return (
             <div className="container">
+                <Navigation />
+                <ThemeSetter />
                 <h1>Links</h1>
                 <p>
                     This is the links page.
